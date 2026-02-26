1dfe9ad4b85ae6fa7226b2d25ef4027c7e77ef38
sudhir-kumar999 <sk2001may@gmail.com>
2026-02-26 10:25:48 +0530

    cookies

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -82,6 +82,7 @@ export const login = async (req, res) => {
       secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 24 * 60 * 60 * 1000, // 1 day
+      path: "/"
     });
 
     return res.json({
